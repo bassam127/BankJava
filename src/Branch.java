67df6e9cdcd0c8791e76bbbf6e36ebe45039ebc2
bassam <bassam.aldalati@fast2.se>
2017-08-08 15:44:09 +0200

    add some java doc

--- src/Branch.java
+++ src/Branch.java
@@ -11,7 +11,13 @@ public class Branch {
         this.name = name;
         this.customers = new ArrayList<Customer>();
     }
-
+/**
+ * 
+ * @param customerName
+ * @param initAmount
+ * @return true if customer not exist and add as new customer 
+ * @return false if customer already exist 
+ */
     public boolean addCustomer(String customerName, double initAmount){
         if(findCustomer(customerName)==null){
             this.customers.add(new Customer(customerName, initAmount));
@@ -19,7 +25,13 @@ public class Branch {
         }
         return false;
     }
-
+    /**
+     * 
+     * @param customerName
+     * @param amount
+     * @return true if customer exist and add amount of money
+     * @return false if customer doesn't exist 
+     */
     public boolean addCustomerTransaction(String customerName , double amount){
 
         Customer existingCustomer = findCustomer(customerName);
@@ -30,7 +42,11 @@ public class Branch {
         }
         return false;
     }
-
+/**
+ * @param customerName
+ * @return  the customer name if customer exist other wise 
+ * @return null 
+ */
     private Customer findCustomer (String customerName){
         for(int i =0 ; i<customers.size(); i++){
             Customer checkCustomer =this.customers.get(i);
